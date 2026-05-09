7ced69ea902c46bf0b144948b09345d2bbb346b7
marlon <mesm252240@gmail.com>
2026-05-08 22:16:34 -0600

    Se modifico el archivo

--- subir.php
+++ subir.php
@@ -5,7 +5,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['imagen'])) {
     $nombre_mostrar = mysqli_real_escape_string($conexion, $_POST['nombre_personalizado']);
     $nombre_archivo = $_FILES['imagen']['name'];
     $ruta_temporal = $_FILES['imagen']['tmp_name'];
-    $carpeta_destino = "img/"; // 👈 CAMBIADO
+    $carpeta_destino = "img/"; // 
 
     if (!file_exists($carpeta_destino)) {
         mkdir($carpeta_destino, 0777, true);
