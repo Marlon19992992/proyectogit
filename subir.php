7bcd0535cd2e2e2e399835c5a20039b61a1aa352
marlon <mesm252240@gmail.com>
2026-05-10 01:53:44 -0600

    Se modifico el archivo

--- subir.php
+++ subir.php
@@ -6,7 +6,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $nombre = $_POST['nombre_personalizado'];
     $archivo = $_FILES['imagen'];
 
-    $carpeta = "img/";
+    $carpeta = "img/"; // 👈 IMPORTANTE
 
     if (!file_exists($carpeta)) {
         mkdir($carpeta, 0777, true);
@@ -15,9 +15,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $ruta = $carpeta . time() . "_" . $archivo['name'];
 
     if (move_uploaded_file($archivo['tmp_name'], $ruta)) {
+
         mysqli_query($conexion, "INSERT INTO imagenes (nombre, ruta) VALUES ('$nombre', '$ruta')");
+
+        echo "Subida OK"; // 👈 DEBUG
+    } else {
+        echo "Error al subir archivo"; // 👈 CLAVE
     }
 }
-
-header("Location: admin.php");
 ?>
