daf6dce642f8f0e9ca40f1a1a650d47e48ff25bb
marlon <mesm252240@gmail.com>
2026-05-10 01:12:52 -0600

    Se agregaron varias lineas de comando

--- login.php
+++ login.php
@@ -1,22 +1,42 @@
 <?php
 session_start();
+include 'db.php';
 
-if ($_POST) {
-    $user = $_POST['usuario'];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $user = $_POST['username'];
     $pass = $_POST['password'];
 
-    if ($user == "admin" && $pass == "1234") {
+    $query = "SELECT * FROM usuarios WHERE username='$user'";
+    $res = mysqli_query($conexion, $query);
+    $data = mysqli_fetch_assoc($res);
+
+    if ($data && password_verify($pass, $data['password'])) {
         $_SESSION['usuario'] = $user;
         header("Location: admin.php");
         exit();
     } else {
-        echo "Credenciales incorrectas";
+        echo "<div class='alert alert-danger text-center'>Credenciales incorrectas</div>";
     }
 }
 ?>
 
-<form method="POST">
-    <input type="text" name="usuario" placeholder="Usuario"><br>
-    <input type="password" name="password" placeholder="Password"><br>
-    <button type="submit">Entrar</button>
-</form>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+
+<body>
+<div class="container">
+    <h2>Login</h2>
+
+    <form method="POST">
+        <input type="text" name="username" placeholder="Usuario" required>
+        <input type="password" name="password" placeholder="Contraseña" required>
+        <button type="submit">Entrar</button>
+    </form>
+</div>
+</body>
+</html>
