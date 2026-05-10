767bb17621c756dd81591da0d3169c11f3f4b96f
marlon <mesm252240@gmail.com>
2026-05-10 01:02:46 -0600

    Se modifico la estrucutura

--- db.php
+++ db.php
@@ -1,7 +1,12 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 $conexion = mysqli_connect("localhost", "root", "", "carrusel");
 
 if (!$conexion) {
     die("Error de conexión: " . mysqli_connect_error());
 }
+
+echo "Conexión exitosa";
 ?>
