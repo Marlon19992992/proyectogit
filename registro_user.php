06babb11e57eb587d4cfae16353f5293dff6186b
marlon <mesm252240@gmail.com>
2026-05-11 02:19:05 -0600

    Se hicieron algunos cambios

--- registro_user.php
+++ registro_user.php
@@ -26,7 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<link rel="stylesheet" href="css/estilo.css">
+<link rel="stylesheet" href="estilo.css">
 
 <body class="bg-dark text-white">
 
