76da8a51c9fb341ac699942c409bfd2595669997
marlon <mesm252240@gmail.com>
2026-05-11 07:37:39 -0600

    Se modifico el archivo

--- registro_user.php
+++ registro_user.php
@@ -26,27 +26,29 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<link rel="stylesheet" href="estilo.css">
 
-<body>
+<body class="bg-dark text-white">
 
-<<div class="login-wrapper">
-    <div class="cyber-card">
+<div class="container mt-5" style="max-width:400px;">
+    <div class="card p-4 bg-black text-white shadow">
 
         <h3 class="text-center mb-3">Nuevo Usuario</h3>
 
         <form method="POST">
-            <input type="text" name="username" class="cyber-input mb-3" placeholder="Usuario" required>
+            <input type="text" name="username" class="form-control mb-3" placeholder="Usuario" required>
 
-            <input type="password" name="password" class="cyber-input mb-3" placeholder="Contraseña" required>
+            <input type="password" name="password" class="form-control mb-3" placeholder="Contraseña" required>
 
-            <button class="cyber-btn" type="submit">Registrar</button>
+            <button class="btn btn-success w-100">Registrar</button>
         </form>
 
-        <a href="login.php" class="btn-link">
+        <a href="login.php" class="btn btn-link text-info mt-2">Ir al login</a>
 
     </div>
 </div>
 
 </body>
 </html>
+
+</html>
+
